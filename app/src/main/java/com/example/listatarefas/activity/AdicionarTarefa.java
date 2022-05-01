e23c7931f1a0c8690333ff3d10c1364a3bb8e372
TiagoAndreAlmeida <thyagoeap@gmail.com>
2022-05-01 10:45:00 -0300

    Add menu action on second actived

--- app/src/main/java/com/example/listatarefas/activity/AdicionarTarefa.java
+++ app/src/main/java/com/example/listatarefas/activity/AdicionarTarefa.java
@@ -1,8 +1,12 @@
 package com.example.listatarefas.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.listatarefas.R;
 
@@ -12,5 +16,22 @@ public class AdicionarTarefa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_tarefa);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Integer menuId = item.getItemId();
+        switch (menuId) {
+            case R.id.salvar_action:
+                Toast.makeText(getApplicationContext(), "Salvo", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/listatarefas/helper/DBHelper.java
@@ -0,0 +1,4 @@
+package com.example.listatarefas.helper;
+
+public class DBHelper {
+}
